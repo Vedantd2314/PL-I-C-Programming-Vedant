e17a6a42dd492d0198dffe851db4efc05722f729
Vedantd2314 <vedantd005@gmail.com>
2026-09-16 12:51:02 +0530

    Remove student information handling from VTD2.c
    Removed student information input and output code.

--- VTD2.c
+++ VTD2.c
@@ -24,20 +24,4 @@ int main()
 
     return 0;
     }
-    int rollnum;
-    float per;
-    char grade;
-     printf("\n Enter STudent Roll NUmber: ");
-     scanf(" %d", &rollnum);
-     printf("\n Enter Student Percentage: ");
-     scanf(" %f", &per);
-     printf("\n Enter STUdent Grade: ");
-     scanf(" %c",&grade);
-      
-      printf("-----Student information-----\n");
-    printf("\n Roll number is : %d",rollnum);
-    printf("\n Percentage is : %f",per);
-    printf("\n Grade is : %c",grade);
-
-    return 0;
-    }
+    
